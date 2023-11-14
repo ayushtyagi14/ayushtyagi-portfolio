afffdc3f9dfc17d6b9fbac347b50b2c340853cf0
Ayush Tyagi <ayush.tyagi145@gmail.com>
2023-11-14 23:56:13 +0530

    added gatherly project

--- components/Homepage/Work.jsx
+++ components/Homepage/Work.jsx
@@ -6,25 +6,25 @@ const Work = () => {
   const work = [
     {
       id: 1,
+      title: "Gatherly Social Media App",
+      videoUrl:
+        "https://res.cloudinary.com/dagkenafn/video/upload/v1699982133/gatherly_hm7ipb.mp4",
+      description:
+        "Gatherly, a dynamic social media website reminiscent of Instagram, boasts a tech-savvy stack comprising React and Typescript for the frontend, ensuring a sleek and responsive user interface. The backend and database storage are expertly handled by Appwrite, offering a robust foundation for seamless data management and retrieval. Users can effortlessly create accounts, share vivid moments with details like captions and locations, and explore a captivating infinite-scrolling explore page.",
+      techstack: ["ReactJs", "Typescript", "TailwindCSS", "Appwrite"],
+      visit: "https://gatherly-app.vercel.app/",
+      code: "https://github.com/ayushtyagi14/gatherly",
+    },
+    {
+      id: 2,
       title: "ShirtX Customizer Website",
       videoUrl:
         "https://res.cloudinary.com/dagkenafn/video/upload/v1693477219/shirtx_fqwiit.mp4",
       description:
-        "ShirtX is a dynamic website built with Vite and Node.js, featuring a 3D t-shirt model that allows users to customize their shirts with logos and graphics. Powered by Dall-e OpenAI integration, it enables AI-generated logo and graphic creation. Explore limitless possibilities in shirt customization.",
+        "ShirtX, an innovative website crafted with Vite and Node.js, introduces a dynamic experience in the realm of online fashion. Central to its uniqueness is a 3D t-shirt model, inviting users to personalize their shirts with logos and graphics.ShirtX seamlessly integrates with DALL-E OpenAI, unleashing the power of AI-generated logo and graphic creation. This synergy of Vite, Node.js, ThreeJs and OpenAI brings forth a platform that transcends conventional shirt customization, offering users a limitless canvas for creative expression.",
       techstack: ["Vite", "NodeJs", "ThreeJs", "TailwindCSS"],
       visit: "https://shirtx.vercel.app/",
       code: "https://github.com/ayushtyagi14/shirtx",
-    },
-    {
-      id: 2,
-      title: "ThoughtSpot Posts Website",
-      videoUrl:
-        "https://res.cloudinary.com/dagkenafn/video/upload/v1693477219/thoughtspot_m3hi0z.mp4",
-      description:
-        "This is a social interaction website that is fully functional and responsive. The app includes Google Authorization via Firebase. The site allows you to post any post, leave comments on other people's posts, and interact with other users. The website is built using NextJS and the database and authorization are handled by Firebase.",
-      techstack: ["NextJs", "Firebase", "TailwindCSS", "GoogleAuth"],
-      visit: "https://thought-spot.vercel.app/",
-      code: "https://github.com/ayushtyagi14/ThoughtSpot",
     },
     {
       id: 3,
@@ -32,7 +32,7 @@ const Work = () => {
       videoUrl:
         "https://res.cloudinary.com/dagkenafn/video/upload/v1693477231/dashboard_yme1dx.mp4",
       description:
-        "This is an Admin Panel Website that is fully responsive and fully functional. A dashboard, three pages, four apps, and seven fully functional charts are included in this admin panel. ReactJS is used to build this website, as well as syncfusion to build all the chart components.",
+        "This Admin Panel Website sets a new standard with its full responsiveness and functionality. Powered by ReactJS, it encompasses a comprehensive dashboard, three pages, and four apps, ensuring a seamless user experience. Elevating data visualization, the panel integrates seven fully functional charts, all meticulously crafted using Syncfusion components. The combination of ReactJS and Syncfusion empowers the admin panel with a powerful and dynamic interface, delivering a robust solution for efficient data management and visualization.",
       techstack: ["ReactJs", "TailwindCSS", "Syncfusion", "JavaScript"],
       visit: "https://admin-dashboard-ayushtyagi14.vercel.app/",
       code: "https://github.com/ayushtyagi14/admin-dashboard",
@@ -43,7 +43,7 @@ const Work = () => {
       videoUrl:
         "https://res.cloudinary.com/dagkenafn/video/upload/v1693477226/basshub_we0ow2.mp4",
       description:
-        "This is a fully functional Ecommerce Website where you can seamlessly select and add items to your cart, and then proceed to a secure and efficient payment process. Developed using NextJs offers a smooth and engaging browsing experience. Powered by Stripe, ensuring the highest transaction security.",
+        "Embark on a seamless shopping journey with our fully functional Ecommerce Website. Crafted with NextJs, it provides a smooth and engaging browsing experience, allowing users to effortlessly select and add items to their cart. The platform prioritizes security, integrating Stripe for a secure and efficient payment process. With a user-friendly interface and robust backend, our Ecommerce Website not only simplifies the shopping experience but also guarantees the highest transaction security, ensuring a confident and enjoyable online shopping venture for every user.",
       techstack: ["NextJs", "TailwindCSS", "Sanity CMS", "Stripe Payments"],
       visit: "https://basshub-ecommerce.vercel.app/",
       code: "https://github.com/ayushtyagi14/basshub_ecommerce",
@@ -77,7 +77,9 @@ const Work = () => {
             <h1 className="lg:text-[30px] text-[26px] text-center font-bold text-white">
               {item.title}
             </h1>
-            <p className="font-dm font-thin">{item.description}</p>
+            <p className="font-dm font-light w-[90%] leading-6 text-[14px]">
+              {item.description}
+            </p>
             <div className="flex lg:flex-row flex-col gap-5 lg:items-center items-start justify-between w-[90%] mt-4">
               <Button url={item.visit} title={"Visit"} outerWidth={"15em"} />
               <Button url={item.code} title={"Code"} outerWidth={"15em"} />
