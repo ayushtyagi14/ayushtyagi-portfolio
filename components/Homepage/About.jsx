cd9205b687928c89a87ec0306e78ed73ecec0934
Ayush Tyagi <ayush.tyagi145@gmail.com>
2024-11-22 19:57:08 +0530

    Update About.jsx

--- components/Homepage/About.jsx
+++ components/Homepage/About.jsx
@@ -76,7 +76,7 @@ const About = () => {
               challenging adventures.
             </p>
             <Button
-              url={"https://drive.google.com/file/d/1NETaWy8Dky1TlZE9bxu26Scbt6kQBStD/view?usp=sharing"}
+              url={"https://drive.google.com/file/d/1WbVLwjiRuxCve-xVPrKzFtNeZiWZJVPt/view?usp=sharing"}
               title={"My Resume"}
               outerWidth={"15em"}
             />
